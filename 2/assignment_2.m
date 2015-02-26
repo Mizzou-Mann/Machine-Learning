ea0e1b85fd764ebe3e1b0356f77efaf6809ee459
Mann <chanmannlim@gmail.com>
2015-02-25 20:01:05 -0600

    plot sample data in dataset1, decision boundary and g(x_n)

--- 2/assignment_2.m
+++ 2/assignment_2.m
@@ -9,7 +9,7 @@
 % Homework Assignment 2
 % Problem 4
 %
-% clc; clear; close all;
+clc; clear; close all;
 dataset1 = load('2DGaussianDataset1.txt');
 dataset2 = load('2DGaussianDataset2.txt');
 
@@ -20,4 +20,24 @@ dataset2 = load('2DGaussianDataset2.txt');
 %
 % c - classify dataset1 with map1
 %
-classification_1 = map1(dataset1);
+[w1, w2, gxs] = map1(dataset1);
+disp('w1 = '); disp(w1);
+disp('w2 = '); disp(w2);
+
+%
+% d - plot data sample and decision boundary
+%
+x = -3:2;
+y = -3/2*x + 5/4;
+figure;
+plot(w1(:,1), w1(:,2), '.r'); hold on;
+plot(w2(:,1), w2(:,2), 'ok'); hold on;
+plot(x, y); hold off;
+legend('w1', 'w2', 'decision boundary');
+title('Dataset1');
+
+%
+% e - plot g(x_n)
+%
+figure;
+plot(gxs);
--- 2/map1.m
+++ 2/map1.m
@@ -1,4 +1,4 @@
-function [ categories ] = map1( dataset )
+function [ w1, w2, gxs ] = map1( dataset )
 % Mininum error classifier for 2-D Gaussian with
 %   Mean:
 %       u1 = [1; 3]
@@ -9,23 +9,35 @@ function [ categories ] = map1( dataset )
 %
 %   where c = 1.21 and I = identity matrix
 %
-% Input:    dataset - dataset to be classified
-% Output:   classes - output classification : 1 for class (w1) and 0 for class (w2)
+% Input:    
+%   dataset - dataset to be classified
+%
+% Output:   
+%   w1 - data classified as w1
+%   w2 - data classified as w2
 
     u1 = [1; 3]; u2 = [-2; 1];
     c = 1.21;
     w = 1/c * (u1 - u2);
     x0 = 1/2 * (u1 + u2);
 
-    categories = [];
+    gxs = [];
+    % NOTE: linear algebra can replace "for loop" here
+    %       see ENHANCED section below
     for i=1:size(dataset, 1)
         x = dataset(i, :)';
         g = w' * (x - x0);
-        categories(i) = g > 0;
+        gxs(i) = g;
     end
+    
+    classification = gxs > 0;
+    w1 = dataset(classification == 1, :);
+    w2 = dataset(classification == 0, :);
+end
 
+% ENHANCED:
+%
 %     scaled_x0 = x0 * ones(1, size(dataset, 1));
 %     G = w' * (dataset' - scaled_x0);
 %     categories = G > 0;
-end
 
